e46585d045412e81eddd11ee343b713341bd2b61
Sergio <sergio.pm.developer@gmail.com>
2024-11-01 14:10:48 +0000

    feat: implement password model

--- apps/passwords/models.py
+++ apps/passwords/models.py
@@ -1,1 +1,39 @@
 from django.db import models
+
+from authentication.models import AppUser
+
+
+class Password(models.Model):
+    """
+    Password model
+    """
+
+    user = models.ForeignKey(
+        AppUser, on_delete=models.CASCADE, related_name="passwords"
+    )
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    site = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        """
+        String representation of the password model
+        """
+
+        return self.name
+
+    def user_email(self):
+        """
+        Returns the email of the user related to the password
+        """
+
+        return self.user.email
+
+    class Meta:
+        """
+        Metadata options
+        """
+
+        db_table = "passwords"
