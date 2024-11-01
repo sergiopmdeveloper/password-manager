6b6a1839e98c388e682a2945b1dc36350bd8c59f
Sergio <sergio.pm.developer@gmail.com>
2024-11-01 18:00:18 +0000

    feat: implement copy password functionality

---
+++ static/js/passwords/index.js
@@ -0,0 +1,27 @@
+/**
+ * Copies the password to the clipboard when
+ * the user clicks the copy password button
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const copyPasswordButtons = document.querySelectorAll(
+    '#copy-password-button'
+  );
+
+  copyPasswordButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+      const password = this.getAttribute('data-password');
+
+      navigator.clipboard.writeText(password).then(() => {
+        this.textContent = 'Copied';
+        this.classList.add('opacity-50', 'cursor-not-allowed');
+        this.disabled = true;
+
+        setTimeout(() => {
+          this.textContent = 'Copy';
+          this.classList.remove('opacity-50', 'cursor-not-allowed');
+          this.disabled = false;
+        }, 2000);
+      });
+    });
+  });
+});
